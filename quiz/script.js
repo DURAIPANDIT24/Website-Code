1f07160fb423d42c6f4b76f7bba39045ad7b7ccb
DURAIPANDIT24 <76253037+DURAIPANDIT24@users.noreply.github.com>
2021-05-06 21:05:25 +0530

    dd

--- quiz/script.js
+++ quiz/script.js
@@ -1,4 +1,4 @@
-var index,board,qus,correct=0;
+var index=0,index,board,qus,correct=0,f=1;
 var questions = {
 	1:{	"question":"230-30","optionA":"45","optionB":"23","optionC":"30","correctAnswer":"c" },
 	2:{	"question":"200+100","optionA":"300","optionB":"301","optionC":"302","correctAnswer":"a" },
@@ -9,20 +9,21 @@ var questions = {
 
 function display() {
 	board = document.getElementById("board");
-	if(pos>=questions.length) {
+	if(index>=questions.length) {
 		
-		board.innerHTML="<h2>You Got "+correct+" of "+questions.length+"</h2>"
+		board.innerHTML="<h2>You Got "+correct+" of "+Object.keys(questions).length+"</h2>"
 		document.getElementById("status").innerHTML="Quiz completed";
 		index=0;
 		correct=0;
 		return false;
 	}
-	document.getElementById("status").innerHTML="Questions "+(index+1)+" of "+questions.length+" ";
-	board.innerHTML="<h3>"+question[index].question+"</h3>";
-	board.innerHTML+="<label> <input type='radio' name='choice' checked='checked' value='optionA'>"+question[index].optionA+"</label>";
+	document.getElementById("status").innerHTML="Questions "+(index+1)+" of "+Object.keys(questions).length+"";
+	board.innerHTML="<h3>"+questions[f].question+"</h3>";
+	board.innerHTML+="<label> <input type='radio' name='choice' checked='checked' value='optionA'>"+question[f].optionA+"</label>";
 	board.innerHTML+="<label> <input type='radio' name='choice' value='optionB'>"+question[index].optionB+"</label>";
 	board.innerHTML+="<label> <input type='radio' name='choice' value='optionC'>"+question[index].optionC+"</label>";
 	board.innerHTML+="<button onclick='checkAnswer()'>submit</button>";
+
 }
 
 function checkAnswer() {
