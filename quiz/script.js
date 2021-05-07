d53962102eca4c029d9d6fa18a45b10b2efb9507
DURAIPANDIT24 <76253037+DURAIPANDIT24@users.noreply.github.com>
2021-05-07 11:24:13 +0530

    json array syntax

--- quiz/script.js
+++ quiz/script.js
@@ -1,11 +1,10 @@
-var index=0,index,board,qus,correct=0,f=1;
-var questions = {
-	1:{	"question":"230-30","optionA":"45","optionB":"23","optionC":"30","correctAnswer":"c" },
-	2:{	"question":"200+100","optionA":"300","optionB":"301","optionC":"302","correctAnswer":"a" },
-	3:{	"question":"100/10","optionA":"11","optionB":"10","optionC":"12","correctAnswer":"b" },
-	4:{	"question":"20-10","optionA":"11","optionB":"12","optionC":"10","correctAnswer":"c" },
-	5:{	"question":"10+100","optionA":"110","optionB":"11","optionC":"12","correctAnswer":"a" },
-}
+var index=0,index,board,qus,correct=0,f=0,answer;
+var questions = [
+	{"question":"200+100","optionA":"300","optionB":"301","optionC":"302","correctAnswer":"optionA" },
+	{"question":"100/10","optionA":"11","optionB":"10","optionC":"12","correctAnswer":"optionB" },
+	{"question":"20-10","optionA":"11","optionB":"12","optionC":"10","correctAnswer":"optionC" },
+	{"question":"11+100","optionA":"111","optionB":"11","optionC":"12","correctAnswer":"optionA" }
+]
 
 function display() {
 	board = document.getElementById("board");
@@ -18,19 +17,22 @@ function display() {
 		return false;
 	}
 	document.getElementById("status").innerHTML="Questions "+(index+1)+" of "+Object.keys(questions).length+"";
-	board.innerHTML="<h3>"+questions[f].question+"</h3>";
-	board.innerHTML+="<label> <input type='radio' name='choice' checked='checked' value='optionA'>"+question[f].optionA+"</label>";
-	board.innerHTML+="<label> <input type='radio' name='choice' value='optionB'>"+question[index].optionB+"</label>";
-	board.innerHTML+="<label> <input type='radio' name='choice' value='optionC'>"+question[index].optionC+"</label>";
+	
+	board.innerHTML="<h3>"+questions[index].question+"</h3>";
+	board.innerHTML+="<label> <input type='radio' name='choice' value='optionA'>"+questions[index].optionA+"</label>";
+	board.innerHTML+="<label> <input type='radio' name='choice' value='optionB'>"+questions[index].optionB+"</label>";
+	board.innerHTML+="<label> <input type='radio' name='choice' value='optionC'>"+questions[index].optionC+"</label>";
 	board.innerHTML+="<button onclick='checkAnswer()'>submit</button>";
+
+
 
 }
 
 function checkAnswer() {
 	choice=document.getElementsByName("choice");
-	for(var i=0;i<choice.length;i++) {
-		if(choice[i].checked) {
-			answer=choice[i].value;		
+	for(var j=0;j<choice.length;j++) {
+		if(choice[j].checked) {
+			answer=choice[j].value;		
 		}
 	}
 	if (answer == questions[index].correctAnswer) {
@@ -38,4 +40,5 @@ function checkAnswer() {
 	}
 index++;
 display();
+
 }
